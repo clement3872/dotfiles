2d15b043ce188911f7401aa9d0c62a4bf68226f3
clement3872 <clement.noisy78@gmail.com>
2023-10-09 23:58:16 +0200

    rename scripts/partitionning to mounting + update mounting/*

--- scripts/mounting/mount_ntfs_partitions.py
+++ scripts/mounting/mount_ntfs_partitions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# you'l need sudo password will running this file.
+# this script mounts all ntfs and ext4 partitions on your system
+# you'll need sudo password will running this file.
 
 import os
 
@@ -32,17 +33,23 @@ def get_disk_name(disk):
         # the 2nd index of it, without odd characters
         name = ''.join(e for e in disk.split('"')[1] if e.isalnum())
     else:
-        name = get_disk_path(disk)
+        name = ''.join(e for e in get_disk_path(disk) if e.isalnum())
+
 
     return name
 
 
+# mount ntfs partitions
 disk_info = os.popen('sudo blkid | grep ntfs').read().split('\n')[:-1]
-
 disk_info = [(get_disk_path(disk), get_disk_name(disk)) for disk in disk_info]
-
-
-
 for path,name in disk_info:
     os.system(f"sudo mkdir -p /media/{name}_mounted/")
     os.system(f"sudo mount -t ntfs-3g {path} /media/{name}_mounted/")
+
+
+# mount ext4 partitions
+disk_info = os.popen('sudo blkid | grep ext4').read().split('\n')[:-1]
+disk_info = [(get_disk_path(disk), get_disk_name(disk)) for disk in disk_info]
+for path,name in disk_info:
+    os.system(f"sudo mkdir -p /media/{name}_mounted/")
+    os.system(f"sudo mount -t ext4 {path} /media/{name}_mounted/")
